efb42c2c89a05b5a8565b5b82a1658ef60ec3a74
יחיאל ומירי <m0556780173@gmail.com>
2024-01-03 22:26:27 +0200

    my commit message

--- classroom_managment.py
+++ classroom_managment.py
@@ -42,21 +42,39 @@ def add_student(name, email=None):
              in lowercase, you can use the `s.lower()` method
     'grade': initialize with empty list
     """
+    student = {"name":name, "grades":[]}
+    student ["email"]=email or f'{name.lower()}@example.com'
+    classroom.append(student)
     pass
 
 
 def delete_student(name):
     """Delete a student from the classroom"""
+    filtered_classroom = []
+    for student in classroom:
+        if name != student['name']:
+           filtered_classroom.append(student)
+    classroom = filtered_classroom
+
     pass
 
 
 def set_email(name, email):
     """Sets the email of the student"""
+    for student in classroom:
+        if name == student['name'] and email == student['email']:
+            return student
+
     pass
+
 
 
 def add_grade(name, profession, grade):
     """Adds a new grade to the student grades"""
+    for student in classroom:
+        if name == student['name']:
+            student.grades.Add(profession, grade)
+
     pass
 
 
@@ -64,9 +82,26 @@ def avg_grade(name, profession):
     """Returns the average of grades of the student
     in the specified profession
     """
+    for student in classroom:
+            if name == student['name']:
+                count=0
+                sum_grade=0
+                for grade in profession:
+                   sum_grade=sum_grade+grade
+                   count+=1
+    return (float) (sum_grade/count)
+                
+
     pass
 
 
 def get_professions(name):
     """Returns a list of unique professions that student has grades in"""
+    for student in classroom:
+        if name == student['name']:
+            new_profession=[]
+            for i in student.grades:
+                new_profession.append(i)
+    return new_profession
+
     pass
