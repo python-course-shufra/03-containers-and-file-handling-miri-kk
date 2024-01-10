4deed7d812317d8637f153e379fe2c4eaf67529a
יחיאל ומירי <m0556780173@gmail.com>
2024-01-10 23:41:29 +0200

    1

--- quiz_app.py
+++ quiz_app.py
@@ -3,19 +3,41 @@ import random
 
 
 def main():
-    # TODO: your code here
 
-    # 1. Get the command line arguments via sys.argv
+    nameFile=sys.argv[1]
+    subject=int(sys.argv[2])
 
-    # 2. Open the correct file open(rf'questions\<filename>.txt)'
+    with open (rf'questions\{nameFile}.txt','r') as file:
+        count_answer_correct=0
 
-    # 3. Iterate over the file
+        
+        for _ in range(subject):
+            line = file.readline().split(";")
+            question=line[0].strip()
+            correct_answer=int(line[1].strip())
+            answers = [answer.strip() for answer in line[2].split(",")]
+            answers.append(correct_answer)
+            random.shuffle(answers)
+        # הדפסת השאלה
+            print (question)
+            
+            # הדפסת התשובות
+            lst=[1,2,3,4]
+            dictionary = {i+1: answer for i, answer in enumerate(answers)}
+            
+            for index, answer in dictionary.items():
+                print(f"{index}. {answer}") 
 
-    #       3.1. Parse the line (use s.split())
-    #       3.2 Create a list of options
-    #       3.3 random.shuffle(l)
-    pass
+            correct_key = next(key for key, value in dictionary.items() if value == correct_answer)   
+            
+            # בדיקה והדפסת התשובה
+            num_answer=int (input("Select the correct answer: "))
 
+            if num_answer==correct_key:
+                count_answer_correct+=1
+        print (f'you answered {count_answer_correct}/{subject} correct answers')
+
+    
 
 if __name__ == '__main__':
     main()
